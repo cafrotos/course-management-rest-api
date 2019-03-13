f92b3bfd997854e651b7db3aed75ccbebff1ceee
minhphuong <nguyenminhphuong@unimarmores.com>
2019-03-13 20:55:19 +0700

    refactor model

--- models/index.js
+++ models/index.js
@@ -6,13 +6,19 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+const DB_URL = process.env.DATABASE_URL;
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (DB_URL) {
+  sequelize = new Sequelize(DB_URL)
+}
+else {
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 }
 
 fs
