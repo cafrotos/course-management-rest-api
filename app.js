b3152f9946950cde61574c19c09f362872afd621
minhphuong <nguyenminhphuong@unimarmores.com>
2019-04-10 23:31:34 +0700

    add cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
